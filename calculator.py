5835508b0f2e847528e8a9bf059db900bffffd79
Python Coder <151621155+MueedPythonCodes@users.noreply.github.com>
2025-04-17 05:45:43 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,65 @@
+import os
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(n1,n2):
+    return n1+n2
+
+def subtract(n1,n2):
+    return n1-n2
+
+def multiply(n1,n2):
+    return n1*n2
+
+def divide(n1,n2):
+    return n1/n2
+
+def calculator():
+    print(logo)
+    n1=float(input("Enter first number:"))
+    operators_dic={
+        "+":add,
+        "-":subtract,
+        "*":multiply,
+        "/":divide
+    }
+
+    for symbols in operators_dic:
+        print(symbols)
+
+    end=True
+
+    while end:
+        operator=input("Pick an operator: ")
+        n2=float(input("Enter next value :"))
+        calculate=operators_dic[operator]
+        answer=calculate(n1,n2)
+        print(f"{n1} {operator} {n2} = {answer}")
+        type=input("Enter y to continue or n to start a new calculation or s to close the calculator: ")
+        if type=="y":
+            n1=answer
+        elif type=="n":
+            end=False
+            calculator()
+        else:
+            end=False
+            os.system('cls')
+            print("CASIO")
+
+calculator()
